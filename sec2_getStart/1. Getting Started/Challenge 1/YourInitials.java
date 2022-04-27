533ca05b0c8bf668a4e014391c6b8c4c212076ff
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-04-28 01:18:44 +0200

    Added Type Conversion lecture to section 4.

---
+++ sec2_getStart/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("TTTTTTTTTTT     JJ");
+        System.out.println("    TT            ");
+        System.out.println("    TT          JJ");
+        System.out.println("    TT          JJ");
+        System.out.println("    TT      JJ  JJ");
+        System.out.println("    TT     JJ   JJ");
+        System.out.println("    TT     JJJJJJ");
+        
+    }
+}
