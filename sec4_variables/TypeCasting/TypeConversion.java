f23087705e4d3568d1fe4982a1288f7e7129318f
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-05-05 13:38:04 +0200

    Complete Casting chapters and task.

---
+++ sec4_variables/TypeCasting/TypeConversion.java
@@ -0,0 +1,14 @@
+public class TypeConversion {
+  /**
+   * Casting in Java
+   */
+public static void main(String[] args) {
+  // Casting (converting double to int)
+  double salary = 5833.3333;
+  System.out.println("Percy's salary is: $" + salary);
+
+  int roundedSalary = (int)salary;
+  System.out.println(roundedSalary);
+
+}
+}
