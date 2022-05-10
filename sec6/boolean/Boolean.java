f788bffa0f464cfed39f5b51e664cb4b753e22d6
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-05-10 13:36:16 +0200

    Fixed a bug in ChatBot which casused output to skip an input question. Completed Boolean lectures, quiz, and task.

---
+++ sec6/boolean/Boolean.java
@@ -0,0 +1,13 @@
+public class Boolean {
+  /**
+   * Boolean practice
+   */
+  public static void main(String[] args) {
+    boolean bool1 = true;
+    boolean bool2 = false;
+
+    System.out.println(bool1);
+    System.out.println(bool2);
+    
+  }
+}
---
+++ sec6/boolean/BooleanComparison.java
@@ -0,0 +1,44 @@
+public class BooleanComparison {
+  /**
+   * 
+   * Boolean comparisons practice
+   */
+  public static void main(String[] args) {
+
+    // int comparisons
+    int chemistryGrade = 95;
+    int biologyGrade = 75;
+
+    System.out.println(chemistryGrade > biologyGrade);
+    System.out.println(chemistryGrade < biologyGrade);
+
+    System.out.println("");
+
+    // double comparisons
+    double sales = 45.55;
+    double costs = 35.47;
+
+    System.out.println(costs < sales);
+
+    // Not equal to  !=
+    System.out.println(sales != costs);
+
+
+    // String comparisons
+    System.out.println("");
+
+    String name1 = "Ali";
+    String name2 = "Muhammad";
+
+    System.out.println(name1.equals(name2));
+
+  // Equals to ==
+  System.out.println("");
+
+  System.out.println(54 == 60);
+  System.out.println(54 == 54);
+  }
+
+
+  
+}
